b20532109ecbd82331ed98651a563c446fe88874
Dominik Keruc <dk53949@fer.hr>
2024-10-28 21:27:15 +0100

    dodani folderi

--- backend/src/main/java/com/apostoli/UnluckyApp/UnluckyApplication.java
+++ backend/src/main/java/com/apostoli/UnluckyApp/UnluckyApplication.java
@@ -1,13 +1,13 @@
-package com.apostoli.backend;
+package com.apostoli.UnluckyApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class UnluckyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(UnluckyApplication.class, args);
 	}
 
 }
