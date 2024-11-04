e857f8c381370bccd3b68625fb29597fd5ad4b90
Dominik Keruc <dk53949@fer.hr>
2024-11-04 02:42:47 +0100

    dodani user

--- backend/src/test/java/com/apostoli/UnluckyApp/BackendApplicationTests.java
+++ backend/src/test/java/com/apostoli/UnluckyApp/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.apostoli.backend;
+package com.apostoli.UnluckyApp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class UnluckyApplicationTests {
 
 	@Test
 	void contextLoads() {
