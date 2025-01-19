d464d646ed281c35db9f73fb63a5f6061ae1c614
MarkoPekasFER <marko.pekas@fer.hr>
2025-01-19 22:56:04 +0100

    bug fixes

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -88,7 +88,7 @@ export default function Home() {
 
 
       </div>
-      <div className="h-screen bg-white">
+      <div className="bg-white">
           <h1 className="font-bold text-2xl text-center pt-10">
             Nesreće HR
           </h1>
@@ -104,14 +104,68 @@ export default function Home() {
             </Link>
           </div>
 
-          <div className="grid xl:grid-cols-2 gap-2 xl:gap-8 p-4">
-            <div>
-
+          <div className="grid xl:grid-cols-2 gap-2 xl:gap-8 p-4 mt-8">
+            <div className="max-w-2xl mx-auto w-full">
+              <div className="w-full relative object-cover pt-[100%] rounded-xl overflow-hidden">
+                <Image src="/hero.jpg"  alt="hero" fill className="object-cover" />
+              </div>
             </div>
-            <div className="w-full relative object-cover pt-[100%] rounded-xl overflow-hidden">
-              <Image src="/hero.jpg"  alt="hero" fill className="object-cover" />
+            <div className="flex flex-col items-center justify-center">
+              <h2 className="font-bold text-xl pt-5 text-center">
+                Za sigurnu i informiranu Hrvatsku
+              </h2>
+              <p className="text-center text-lg pt-5 max-w-xl mx-auto">
+              Ostanite sigurni i informirani o svim nesrećama u Hrvatskoj. Prijavite nesreću i pomozite drugima.
+              </p>
             </div>
           </div>
+
+          <div className="py-24">
+            <h2 className="font-bold text-2xl text-center pt-10">
+              Kako koristiti aplikaciju
+            </h2>
+            <p className="text-center text-lg pt-5 max-w-xl mx-auto">
+              Prijavite nesreću i vidite sve nesrece u blizini pomocu 
+            naše aplikacije. Vaša sigurnost nam je na prvom mjestu, stoga budite oprezni i odgovorni.
+            </p>
+          </div>
+
+          <div className=" mb-12">
+            <h2 className="font-bold text-2xl text-center pt-10">
+              Zelite pomoci?
+            </h2>
+            <p className="text-center text-lg pt-5 max-w-xl mx-auto">
+              Sve drzavne i privatne institucije koje zele pomoci ljudima u nesreci se mogu javiti na
+              email: slu@unlucky.hr
+            </p>
+            <p className="text-center text-lg pt-5 max-w-xl mx-auto">
+              Povratno cemo se javiti ako vjerujemo da mozemo zajedno pomoci.
+            </p>
+          </div>
+
+          <footer>
+            <div className="bg-gray-800 text-white">
+              <div className="grid md:grid-cols-2">
+                <div className="p-4">
+                  {/* contact details */}
+                  <p className="font-bold">Unlucky d.o.o.</p>
+                  <p>Ulica grada Vukovara 269D</p>
+                  <p>10000 Zagreb</p>
+                  <p>OIB: 12345678901</p>
+                </div>
+                <div className="p-4">
+                  <p className="font-bold">Kontakt</p>
+                  <p>Email: info@unlucky.hr</p>
+                  <p>Telefon: 01 123 4567</p>
+                </div>
+              </div>
+              <div className="container mx-auto py-4">
+                <p className="text-center">
+                  &copy; 2021 Nesreće HR
+                </p>
+              </div>
+            </div>
+          </footer>
       </div>
     </div>
   );
